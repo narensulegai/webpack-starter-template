1f9589805977f799df0a4839e642871b203d1ea1
Naren Sulegai <86naren@gmail.com>
2020-02-13 14:23:52 +0530

    Init commit

---
+++ src/sample.js
@@ -0,0 +1,11 @@
+function calcCredit(rating) {
+  let credit = 0;
+  if (rating > 4 && rating <= 4.5) credit = 1;
+  else if (rating > 4.5) credit = 2;
+  return credit;
+}
+
+
+module.exports = {
+  calcCredit
+};
